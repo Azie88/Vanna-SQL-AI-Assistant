f9d8d01b4affc7e42635effff73ce28bdfc66bf7
Azie88 <obandoandrew8@gmail.com>
2025-02-01 16:49:01 +0300

    Starter Code Update

--- Vanna_AI_Starter.py
+++ Vanna_AI_Starter.py
@@ -4,12 +4,9 @@ import vanna
 from vanna.remote import VannaDefault
 from vanna.flask import VannaFlaskApp
 
-
 load_dotenv()
-
 
 vn = VannaDefault(model='chinook', api_key=os.getenv("vanna_api_key"))
 vn.connect_to_sqlite('https://vanna.ai/Chinook.sqlite')
-
 
 VannaFlaskApp(vn).run()
